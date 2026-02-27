bacfe9ae1d70bb9599e2adc07ce087aab44a84cf
Monitha721 <md9539@srmist.edu.in>
2026-02-27 09:46:10 +0530

    Update UseCase2PalindromeCheckerApp.java

--- UseCase2PalindromeCheckerApp.java
+++ UseCase2PalindromeCheckerApp.java
@@ -1,32 +1,23 @@
-Palindrome Checker App
-The objective of the PalindromeChecker App is to design and implement a console-based Java application that validates whether a given string is a palindrome under different conditions, while strengthening core programming fundamentals and data structure concepts.
+public class UseCase3PalindromeCheckerApp {
 
-UC3: Palindrome Check Using String Reverse
-Goal: Check whether a string is a palindrome by reversing it.
-Flow:
-Reverse string using loop
-Compare original and reversed
-Display result
-Key Concepts used in UC3:
-Loop (for loop) – Used to iterate through the characters of the string in reverse order.
-String Immutability – In Java, String objects are immutable; every modification creates a new String object.
-String Concatenation (+) – Used to build the reversed string character by character (introduced here to later discuss drawbacks).
-equals() Method – Used to compare the actual content of two String objects instead of memory references.
-Data Structure: String
+    public static void main(String[] args) {
 
+        // Original string
+        String word = "level";
 
+        // Variable to store reversed string
+        String reversed = "";
 
+        // Reverse the string using for loop
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversed = reversed + word.charAt(i);
+        }
 
-
-
-
-
-Please refer to the code snapshot below to write your code
-
-Please refer to the hint below to write your code
-
-
-Please first compile the program using javac UseCase3PalindromeCheckerApp.java and run the Program using java UseCase3PalindromeCheckerApp as shown below
-
-
-
+        // Compare original and reversed string
+        if (word.equals(reversed)) {
+            System.out.println(word + " is a Palindrome.");
+        } else {
+            System.out.println(word + " is NOT a Palindrome.");
+        }
+    }
+}
